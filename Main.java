31435637735123e9bca77f013598e48834b98a01
Liana Veldiaeva <lianali108@gmail.com>
2020-06-24 18:50:32 +0200

    java code add

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int  mile  = 20;
+        long amount = 200000;
+
+
+        long bonus = amount / mile ;
+        System.out.println(bonus);
+
+
+    }
+
+}
